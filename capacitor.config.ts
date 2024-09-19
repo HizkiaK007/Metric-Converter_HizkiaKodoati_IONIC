57a697aefbf8049ec5376534f073d36101f1fa35
HizkiaK007 <hizkiakodoati123@gmail.com>
2024-09-19 15:03:53 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Appkonversi',
+  webDir: 'www'
+};
+
+export default config;
